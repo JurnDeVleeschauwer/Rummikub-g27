29fc8ee534f68d0126ab039c3b6c3e3cf22ccaa7
jurnDeVleeschau <jurn.devleeschauwer@student.hogent.be>
2021-02-18 16:02:20 +0100

    Connectie toevoegen

---
+++ src/domein/Connectie.java
@@ -0,0 +1,9 @@
+package domein;
+
+public class Connectie {
+	/**
+	 * Link naar databank met gebuikersnaam en wachtwoord.
+	 */
+    public static final String JDBC_URL = "jdbc:mysql://ID344025_g27.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID344025_g27&password=g27_rummi";
+
+}
