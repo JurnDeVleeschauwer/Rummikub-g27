b4cd6450f8489938e49354d8a246ac55c66b5c3a
Kobe Dehandschutter <Kobe.Dehandschutter@student.hogent.be>
2021-03-10 17:45:44 +0100

    Proberen Speler aan te maken uit Eclipse (nog niet gelukt)

--- src/domein/Connectie.java
+++ src/domein/Connectie.java
@@ -2,6 +2,7 @@ package domein;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Connectie {
@@ -11,15 +12,25 @@ public class Connectie {
 			 public static final String JDBC_URL = "jdbc:mysql://ID344025_g27.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID344025_g27&password=g27_rummi";
 
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://ID344025_g27.db.webhosting.be?serverTimezone=UTC";
-		String user = "ID344025_g27";
-		String password = "g27_rummi";
-				
+			
 		 try 
 		 {
-			 Connection cn = DriverManager.getConnection(url, user, password);
+			 Connection cn = DriverManager.getConnection(JDBC_URL);
+			 
+			 String sql = "insert into Speler(naam, wachtwoord) values(?, ?)";
+			 PreparedStatement stat = cn.prepareStatement(sql);
+			 stat.setString(1, "Piet");
+			 stat.setString(2, "ProbeerUitEclipse");
+			 
+			 int rows = stat.executeUpdate();
+			// if (rows > 0 )
+			//	 System.out.println("Er is iets gebeurt");
+			// stat.close();
+			// cn.close();
+			 
+			 
 		 }catch(SQLException e) {
-			 System.out.println("Oeps! error");
+			 System.out.println("Geen verbinding");
 		 }
 	}
    
