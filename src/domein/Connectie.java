b9a0bed778c637690a67d1e9cdea27230e82c6c9
Kobe Dehandschutter <Kobe.Dehandschutter@student.hogent.be>
2021-03-11 14:55:29 +0100

    Select query geprobeerd (ook niet gelukt) + fout "no database selected" ontdekt en proberen op te lossen (ook nog niet gelukt).

--- src/domein/Connectie.java
+++ src/domein/Connectie.java
@@ -2,8 +2,9 @@ package domein;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Connectie {
 	/**
@@ -12,17 +13,33 @@ public class Connectie {
 			 public static final String JDBC_URL = "jdbc:mysql://ID344025_g27.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID344025_g27&password=g27_rummi";
 
 	public static void main(String[] args) {
-			
+		String url = "jdbc:mysql://ID344025_g27.db.webhosting.be?serverTimezone=UTC";
+		String user = "ID344025_g27";
+		String password ="g27_rummi";
+		
+		
 		 try 
 		 {
-			 Connection cn = DriverManager.getConnection(JDBC_URL);
+			 Connection cn = DriverManager.getConnection(url, user, password);
 			 
-			 String sql = "insert into Speler(naam, wachtwoord) values(?, ?)";
-			 PreparedStatement stat = cn.prepareStatement(sql);
-			 stat.setString(1, "Piet");
-			 stat.setString(2, "ProbeerUitEclipse");
+			 String sql = "SELECT * FROM Speler";
+			 Statement statement = cn.createStatement();
+			 ResultSet result = statement.executeQuery(sql);
 			 
-			 int rows = stat.executeUpdate();
+			 int count = 0;
+			 while (result.next()) {
+				 String naam = result.getString(2);
+				 String wachtwoord = result.getString(5);
+				 count++;
+				 System.out.printf("naam: %s en wachtwoord: %s", naam, wachtwoord);
+			 }
+			 cn.close();
+			// String sql = "insert into Speler(naam, wachtwoord) values(?, ?)";
+			// PreparedStatement stat = cn.prepareStatement(sql);
+			// stat.setString(1, "Piet");
+			// stat.setString(2, "ProbeerUitEclipse");
+			 
+			// int rows = stat.executeUpdate();		// Hier klopt iets niet want deze regel zorgt ervoor dat er geen connectie meer is.
 			// if (rows > 0 )
 			//	 System.out.println("Er is iets gebeurt");
 			// stat.close();
@@ -30,7 +47,7 @@ public class Connectie {
 			 
 			 
 		 }catch(SQLException e) {
-			 System.out.println("Geen verbinding");
+			 System.out.println(e.getMessage());
 		 }
 	}
    
