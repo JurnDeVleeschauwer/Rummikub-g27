6e02c29ed4a024930bb4ded485740cefbd88b7f5
Kobe Dehandschutter <Kobe.Dehandschutter@student.hogent.be>
2021-04-12 11:45:42 +0200

    Spelers kiezen veranderd in buttons

--- src/gui/HoeveelSpelersPaneel.java
+++ src/gui/HoeveelSpelersPaneel.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -37,26 +36,50 @@ public class HoeveelSpelersPaneel extends GridPane {
         setVgap(10);
     }
 
-    private final TextField nummer = new TextField();
+    private int aantalSpelers = 0;
     private final Label foutbericht = new Label();
 
     private void voegComponentenToe() {
-        add(new Label(UITextHelper.UIText("Hoeveel.spelers.spelen.er.mee?.2-4.Spelers")), 0, 1, 1, 1);
-        add(nummer, 1, 1, 1, 1);
+        add(new Label(UITextHelper.UIText("Hoeveel.spelers.spelen.er.mee?.2-4.Spelers")), 0, 1, 4, 1);
 
+        Button spelers2 = new Button("2");
+        spelers2.setOnAction(this::spelers2);
+        add(spelers2, 0, 2, 1, 1);
+        
+        Button spelers3 = new Button("3");
+        spelers3.setOnAction(this::spelers3);
+        add(spelers3, 1, 2, 1, 1);
+        
+        Button spelers4 = new Button("4");
+        spelers4.setOnAction(this::spelers4);
+        add(spelers4, 2, 2, 1, 1);
+        
         Button toevoegen = new Button(UITextHelper.UIText("ok"));
         toevoegen.setOnAction(this::toevoegen);
         add(toevoegen, 0, 4, 2, 1);
 
     }
 
+    private void spelers2(ActionEvent event) {
+    	this.aantalSpelers = 2;
+    }
+    
+    private void spelers3(ActionEvent event) {
+    	this.aantalSpelers = 3;
+    }
+    
+    private void spelers4(ActionEvent event) {
+    	this.aantalSpelers = 4;
+    }
+    
+    
     private void toevoegen(ActionEvent event) {
-        if (nummer.getText().trim().isEmpty()) {
+        if (aantalSpelers == 0) {
             foutbericht.setText(UITextHelper.UIText("Gelieve.een.numer.op.te.geven"));
             return;
 
         } else {
-        		hoofdPaneel.setAantalSpelers(Integer.parseInt(nummer.getText().trim()));
+        		hoofdPaneel.setAantalSpelers(aantalSpelers);
                 hoofdPaneel.toonAanmeldPaneel();;
                 foutbericht.setText(null);
         }
--- src/module-info.java
+++ src/module-info.java
@@ -1,13 +1,14 @@
 module Rummikub {
+	exports persistentie;
+	exports cui;
+	exports gui;
 	exports main;
-	
-	requires javafx.base;
-	requires javafx.graphics;
-	requires javafx.controls;
-	requires javafx.fxml;
+	exports domein;
+	exports exceptions;
+	exports i18n;
+
 	requires java.sql;
-	
-	opens gui to javafx.graphics,javafx.fxml; 
-	opens cui to javafx.graphics,javafx.fxml; 
-	opens domein to javafx.base;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
 }
