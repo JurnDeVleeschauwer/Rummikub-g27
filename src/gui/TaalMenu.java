b0ff61867c95df943ce75a6a8474f82e77802f1a
Jona De Neve <jona.deneve@student.hogent.be>
2021-05-01 12:35:38 +0200

    Vlaggen toegevoegd en tijdelijke effen groene achtergrond

--- src/gui/TaalMenu.java
+++ src/gui/TaalMenu.java
@@ -4,6 +4,10 @@ import i18n.UITextHelper;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 
@@ -27,14 +31,34 @@ public class TaalMenu extends GridPane
         setPadding(new Insets(10));
         setHgap(10);
         setVgap(10);
-        
+        this.setStyle("-fx-background-color: #136032");
     }
 
     
     private void voegComponentenToe()
-    {
-        
-
+    {	
+    	
+    	ImageView nlImage = new ImageView(
+                new Image(getClass().getResourceAsStream("/images/NL_Flag.png"))
+    	);
+    	nlImage.setFitWidth(200);
+    	nlImage.setFitHeight(133);
+    	add(nlImage, 2, 2);
+    	
+    	ImageView frImage = new ImageView(
+                new Image(getClass().getResourceAsStream("/images/FR_Flag.png"))
+    	);
+    	frImage.setFitWidth(200);
+    	frImage.setFitHeight(133);
+    	add(frImage, 3, 2);
+    	
+    	ImageView enImage = new ImageView(
+                new Image(getClass().getResourceAsStream("/images/EN_Flag.png"))
+    	);
+    	enImage.setFitWidth(200);
+    	enImage.setFitHeight(133);
+    	add(enImage, 4, 2);
+    	
         
         Button nl = new Button("Nederlands");
         nl.setOnAction(this::taalkeuze);
@@ -42,7 +66,7 @@ public class TaalMenu extends GridPane
         nl.setPrefHeight(150);
         nl.setPrefWidth(200);
         nl.setFont(new Font(30));
-        add(nl, 1, 3);
+        add(nl, 2, 3);
         
         Button fr = new Button("Français");
         fr.setOnAction(this::taalkeuze);
@@ -50,7 +74,7 @@ public class TaalMenu extends GridPane
         fr.setPrefHeight(150);
         fr.setPrefWidth(200);
         fr.setFont(new Font(40));
-        add(fr, 1, 4);
+        add(fr, 3, 3);
         
         Button en = new Button("English");
         en.setOnAction(this::taalkeuze);
@@ -58,7 +82,7 @@ public class TaalMenu extends GridPane
         en.setPrefHeight(150);
         en.setPrefWidth(200);
         en.setFont(new Font(40));
-        add(en, 1, 5);
+        add(en, 4, 3);
     }
     
     private void taalkeuze(ActionEvent event) {
