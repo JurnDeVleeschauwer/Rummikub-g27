5fe3eb6b4e0dd5ab3b0c4458c319a0299ffee37b
Jona De Neve <jona.deneve@student.hogent.be>
2021-05-14 11:00:31 +0200

    Testklasse RummiSteenTest afgewerkt

--- src/testen/RummiSteenTest.java
+++ src/testen/RummiSteenTest.java
@@ -2,6 +2,8 @@ package testen;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import domein.RummiSteen;
@@ -10,10 +12,36 @@ class RummiSteenTest {
 
 	private RummiSteen r;
 	
-	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void maakRummiSteen_legeConstructor_maaktRummiSteen() {
+		r = new RummiSteen();
+		Assertions.assertEquals(0, r.getWaarde());
+		Assertions.assertEquals("", r.getKleur());
+		Assertions.assertEquals("", r.getNaam());
+	}
+	@Test
+	void maakRummiSteen_geldigeParameters_maaktRummiSteen() {
+		r = new RummiSteen(5,"Blauw");
+		Assertions.assertEquals(5, r.getWaarde());
+		Assertions.assertEquals("Blauw", r.getKleur());
+		Assertions.assertEquals("Blauw5", r.getNaam());
+	}
+	@Test
+	void maakRummiSteen_geldigeParametersJoker_maaktRummiSteen() {
+		r = new RummiSteen(25,"Groen");
+		Assertions.assertEquals(25, r.getWaarde());
+		Assertions.assertEquals("Groen", r.getKleur());
+		Assertions.assertEquals("Joker", r.getNaam());
+	}
+	@Test
+	void toString_geldigeSteen_geeftKorteString() {
+		r = new RummiSteen(5,"Blauw");
+		Assertions.assertEquals("5\nB", r.toString());
+	}
+	@Test
+	void toString_geldigeSteenJoker_geeftKorteString() {
+		r = new RummiSteen(25,"Groen");
+		Assertions.assertEquals("J", r.toString());
 	}
 
 }
